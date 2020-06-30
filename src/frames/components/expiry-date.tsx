e49bcf49e043c819d270bd6873b72d1e52eaecff
Punit Shah <punit.shah@checkout.com>
2020-06-30 19:36:15 +0100

    Use classnames package for component classNames
    Makes it more readable, and fixes issue when used with styled-components

--- src/frames/components/expiry-date.tsx
+++ src/frames/components/expiry-date.tsx
@@ -1,14 +1,8 @@
 import React from 'react';
+import classNames from 'classnames';
 import { EXPIRY_DATE_FRAME } from '../config/config';
 
 export const ExpiryDate: React.FC<React.HTMLAttributes<HTMLDivElement>> = ({
     className,
     ...props
-}) => (
-    <div
-        {...props}
-        className={`${
-            className !== undefined ? +`${className} ${EXPIRY_DATE_FRAME}` : EXPIRY_DATE_FRAME
-        }`}
-    />
-);
+}) => <div className={classNames(EXPIRY_DATE_FRAME, className)} {...props} />;
